61fafa7b80a3139a5c7511ecde1e00ac1561fb40
Samuel Gustin <gustin.daniel1@gmail.com>
2019-08-01 22:02:35 +0300

    Html hierarchy
    Refactored base.html with jinja2
    Added signup, login, play htmls

--- Tetris/views.py
+++ Tetris/views.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, request, redirect, flash, abort
 
 from Tetris import app, db, login_manager
 from .models import User, Game
+from flask_login import current_user, logout_user, login_user, login_required
 
 
 @login_manager.user_loader
@@ -13,3 +14,29 @@ def load_user(userid):
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/play')
+def play():
+    return render_template('play.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return  render_template('signup.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/user/<username>')
+def user(username):
+    pass
